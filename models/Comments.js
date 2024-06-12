8abfa37d8263f90ff295187fd890471e53cb03a0
olamide folayan <84618342+holladredd@users.noreply.github.com>
2024-06-12 18:50:25 +0100

    Add files via upload

--- models/Comments.js
+++ models/Comments.js
@@ -1,28 +1,29 @@
 const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
-  //define data schema
-  bookid: {
-    type: String,
-    required: true,
+const commentSchema = new mongoose.Schema(
+  {
+    //define data schema
+    bookid: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    userid: {
+      type: String,
+      required: false,
+    },
   },
-  username: {
-    type: String,
-    required: true,
-  },
-  comment: {
-    type: String,
-    required: true,
-  },
-  userid: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const CommentModel = mongoose.model("comments", commentSchema);
 module.exports = CommentModel;
